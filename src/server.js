d245b1bc447aa2e196c8c4f7616c854e694c7272
KaranGulve4342 <karangulve4342@gmail.com>
2025-09-13 21:57:56 +0530

    refactor: restructure project to separate app logic and server setup

--- src/server.js
+++ src/server.js
@@ -1,12 +1,6 @@
-import express from 'express';
-
-const app = express();
+import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
