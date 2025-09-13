a2a031f726c1ec2f5abe48c6d25763fee02d2167
KaranGulve4342 <karangulve4342@gmail.com>
2025-09-13 22:35:38 +0530

    feat: add module imports to package.json and integrate logger in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import logger from '#config/logger.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
+  logger.info('Root endpoint accessed');
   res.status(200).send('Hello, World!');
 });
 
